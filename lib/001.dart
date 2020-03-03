298491c4935b14cc2a52e88fd698a6703619fa8e
梁大红 <ios@liangdahong.com>
2020-03-03 23:57:46 +0800

    add

--- lib/001.dart
+++ lib/001.dart
@@ -1,35 +1,35 @@
-// import 'package:flutter/material.dart';
-// void main() {
-//   runApp(MyApp());
-// }
+import 'package:flutter/material.dart';
+void main() {
+  runApp(MyApp());
+}
 
-// // 自定义一个组件 MyApp 继承于 StatelessWidget（无状态的组件）
-// class MyApp extends StatelessWidget {
-// // 这个方法是必须首实现的方法 这里方法返回组件
-//   @override
-//   Widget build(BuildContext context) {
-//     // 这里返回组件
-//     return HomeContent();
-//     }
-// }
+// 自定义一个组件 MyApp 继承于 StatelessWidget（无状态的组件）
+class MyApp extends StatelessWidget {
+// 这个方法是必须首实现的方法 这里方法返回组件
+  @override
+  Widget build(BuildContext context) {
+    // 这里返回组件
+    return HomeContent();
+    }
+}
 
-// // 自定义一个组件 HomeContent 继承于 StatelessWidget（无状态的组件）
-// class HomeContent extends StatelessWidget {
-// // 这个方法是必须首实现的方法 这里方法返回组件
-//   @override
-//   Widget build(BuildContext context) {
-//     // 这里返回组件
-//     return Center(
-//       child: new Text(
-//         "A梁大红",
-//         textDirection: TextDirection.ltr,
-//         textAlign: TextAlign.center,
-//         style: TextStyle(
-//           color: Colors.red,
-//           fontSize: 100,
-//           fontWeight: FontWeight.w900,
-//         ),
-//       ),
-//     );
-//   }
-// }
+// 自定义一个组件 HomeContent 继承于 StatelessWidget（无状态的组件）
+class HomeContent extends StatelessWidget {
+// 这个方法是必须首实现的方法 这里方法返回组件
+  @override
+  Widget build(BuildContext context) {
+    // 这里返回组件
+    return Center(
+      child: new Text(
+        "A梁大红",
+        textDirection: TextDirection.ltr,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: Colors.red,
+          fontSize: 100,
+          fontWeight: FontWeight.w900,
+        ),
+      ),
+    );
+  }
+}
