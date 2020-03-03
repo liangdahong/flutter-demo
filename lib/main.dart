0e1f5323b6c38dd0f905ca1dc1b1d979b7d25845
梁大红 <ios@liangdahong.com>
2020-03-03 10:11:34 +0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// 自定义一个组件 MyApp 继承于 StatelessWidget（无状态的组件）
+class MyApp extends StatelessWidget {
+
+// 这个方法是必须首实现的方法 这里方法返回组件
+  @override
+  Widget build(BuildContext context) {
+
+    // 这里返回组件
+    return Center(
+      child: new Text(
+        "梁大红",
+        textDirection: TextDirection.ltr,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: Colors.red,
+          fontSize: 100,
+          fontWeight: FontWeight.w900,
+        ),
+      ),
+    );
+  }
+}
+
